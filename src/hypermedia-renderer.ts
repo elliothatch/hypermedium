8897478dc966c500bfa1aef48ed020ede28aa6e9
Elliot Hatch <elliot.hatch@gmail.com>
2018-12-17 16:50:51 -0700

    Use handlebars for HTML rendering

--- src/hypermedia-renderer.ts
+++ src/hypermedia-renderer.ts
@@ -5,6 +5,7 @@ import * as Url from 'url';
 import { Observable, Observer } from 'rxjs';
 
 import { NextFunction, Router, Request, Response } from 'express';
+import { compile, registerHelper, SafeString } from 'handlebars';
 
 import { HAL, Hypermedia } from './hypermedia';
 
@@ -12,6 +13,28 @@ export type Html = string;
 export namespace Html {
     export type Link = string;
 }
+
+const template = compile(`
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>{{title}}</title>
+    </head>
+    <body>
+    <div>Links:
+    <ul>
+    {{#each _links}}
+        <li>{{hal-link @key this}}</li>
+    {{/each}}
+    </ul>
+    </div>
+    </body>
+</html>
+`);
+
+// TODO: this doesn't work with link arrays.
+registerHelper('hal-link', (rel, link) => new SafeString(`<a rel=${rel} href=${link.href}>${link.title || link.href}</a>`));
 
 /** Render's HAL into HTML using the handlebars templating engine */
 export class HypermediaRenderer {
@@ -25,29 +48,16 @@ export class HypermediaRenderer {
     }
 
     public render(resource: Hypermedia.ExtendedResource): Html {
-        let links: Html.Link[] = [];
-        if(resource._links) {
-            links = Object.keys(resource._links).reduce((l: Html.Link[], rel) => {
-                const relLinks = resource._links![rel];
-                return Array.isArray(relLinks)?
-                    l.concat(relLinks.map((link) => this.renderLink(rel, link))):
-                    l.concat([this.renderLink(rel, relLinks)]);
-            }, []);
-        }
-        return `
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <title>${resource.title || 'title'}</title>
-            </head>
-            <body>
-            <div>Links:
-            ${links.join('\n')}
-            </div>
-            </body>
-        </html>
-        `;
+        // let links: Html.Link[] = [];
+        // if(resource._links) {
+        //     links = Object.keys(resource._links).reduce((l: Html.Link[], rel) => {
+        //         const relLinks = resource._links![rel];
+        //         return Array.isArray(relLinks)?
+        //             l.concat(relLinks.map((link) => this.renderLink(rel, link))):
+        //             l.concat([this.renderLink(rel, relLinks)]);
+        //     }, []);
+        // }
+        return template(resource);
     }
 
     public renderLink(rel: HAL.Uri, link: HAL.Link): Html.Link {
